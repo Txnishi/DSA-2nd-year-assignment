6276ca27a4e0a5b71bcdb194f99fcbbd14e93998
Tanishi Devlal <104810843+Txnishi@users.noreply.github.com>
2023-01-18 16:24:31 +0530

    Create intersection-of-two-linked-lists.cpp

---
+++ Assignment 9 Hash Map/intersection-of-two-linked-lists.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        unordered_map<ListNode*, int>mp;
+        
+        while(headA != NULL){
+            mp[headA]++;
+            headA = headA->next;
+        }
+
+        while (headB != NULL){
+            if(mp.find(headB) != mp.end()){
+                mp[headB]++;
+                if(mp[headB] > 1){
+                    return headB;
+                }
+            }
+            headB= headB->next;
+        }
+        return NULL;
+        
+    }
+};
