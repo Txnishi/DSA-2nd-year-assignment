a371eaad6d36f222e8dd4efa678bc2af4d14d5ea
Tanishi Devlal <104810843+Txnishi@users.noreply.github.com>
2023-01-27 10:52:13 +0530

    Create first-bad-version.cpp

---
+++ Assignment - 7 (Binary Search)/first-bad-version.cpp
@@ -0,0 +1,28 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        
+        long long int i = 0;
+        long long int len = n-1;
+        
+        while(i<=len){
+            long long int mid = (i+len)/2;
+            
+            if(isBadVersion(mid)){
+                len = mid-1;
+            }
+            
+            if(!isBadVersion(mid)){
+                i = mid+1;
+            }
+            
+        }
+      
+       
+        
+        return i;
+    }
+};
